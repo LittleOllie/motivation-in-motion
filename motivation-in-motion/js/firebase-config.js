eb8ff777e748c23f693a6c6f2d419c9f1a6081c3
LittleOllie <littleollienft@gmail.com>
2026-03-08 14:23:02 +0800

    Delete motivation-in-motion directory

--- motivation-in-motion/js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAQqSZuUfJO-VC9Cblnc00-25vleR_jKpI",
-  authDomain: "motivation-in-motion.firebaseapp.com",
-  projectId: "motivation-in-motion",
-  storageBucket: "motivation-in-motion.firebasestorage.app",
-  messagingSenderId: "913656944921",
-  appId: "1:913656944921:web:a202a07908e4c215d4cd88",
-  measurementId: "G-QWYR19YHLY"
-};
